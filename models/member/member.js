d032f4f91d0887abd56fd073d07b657460747c6f
RosieOh <20172207@gm.hannam.ac.kr>
2024-10-29 01:13:45 +0900

    ✨ FEAT : Member 엔티티 추가 (하다 졸려서 하다 말았음...)

---
+++ models/member/member.js
@@ -0,0 +1,8 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("..");
+
+module.exports = (sequelize, DataTypes) => {
+    const Member = sequelize.define('Member', {
+        // 모델 작성해야하는데 넘 졸려서 낼 해야지
+    })
+}
