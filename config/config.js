0e947dbdb0a085d397abdc4f211b0eb243f52160
RosieOh <20172207@gm.hannam.ac.kr>
2024-10-29 01:06:05 +0900

    ✨ FEAT : DB 설정 로직 구현

---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    username: 'root',
+    password: '1234',
+    database: 'moyang',
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'mariadb'
+  },
+  test: {
+    username: "root",
+    password: "1234",
+    database: "moyang",
+    host: "127.0.0.1",
+    dialect: "mariadb"
+  },
+  production: {
+    username: "root",
+    password: "1234",
+    database: "moyang",
+    host: "127.0.0.1",
+    dialect: "mariadb"
+  }
+};
